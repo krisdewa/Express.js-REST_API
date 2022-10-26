4fc3811b63e32831db39edca535477c0692c40bf
Krisna Dewa <59174531+krisdewa@users.noreply.github.com>
2022-10-26 18:51:15 +0700

    Insert Data into Database

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const port = 3000;
 //     res.send("Hello World!");
 // });
 
+// import library body parser
+const bodyParser = require("body-parser");
+
+// assign library
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 // import route posts
 const postsRouter = require("./routes/posts");
 app.use("/api/posts", postsRouter); // use route posts di Express
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { body, validationResult } = require("express-validator");
+
 // import database connection
 const connection = require("../config/database");
 
@@ -28,5 +30,53 @@ router.get("/", function (req, res) {
         }
     );
 });
+
+/**
+ * STORE POST
+ */
+router.post(
+    "/store",
+    [
+        // validation
+        body("title").notEmpty(),
+        body("content").notEmpty(),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array(),
+            });
+        }
+
+        // define formData
+        let formData = {
+            title: req.body.title,
+            content: req.body.content,
+        };
+
+        // insert query
+        connection.query(
+            "INSERT INTO posts SET ?",
+            formData,
+            function (error, rows) {
+                //if(err) throw err
+                if (error) {
+                    return res.status(500).json({
+                        status: false,
+                        message: "Internal Server Error",
+                    });
+                } else {
+                    return res.status(201).json({
+                        status: true,
+                        message: "Insert Data Successfully",
+                        data: rows[0],
+                    });
+                }
+            }
+        );
+    }
+);
 
 module.exports = router;
